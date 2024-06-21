dafc2f27fcbbae55e90d1a405c1d5d6f2f4172f0
zachsea <zachse4@gmail.com>
2024-06-20 19:17:59 -0700

    Adds basic front end layout and temporary controls

---
+++ sim.config.js
@@ -0,0 +1,104 @@
+// helper variables and functions for the simulation configuration as needed
+const basicAlleles = [
+  { id: 1, type: "coat", brownWeek: 15, brownRate: 1, whiteWeek: 36, whiteRate: 1 },
+  { id: 2, type: "coat", brownWeek: 24, brownRate: 1, whiteWeek: 36, whiteRate: 1 },
+  { id: 3, type: "coat", brownWeek: 26, brownRate: 1, whiteWeek: 36, whiteRate: 1 },
+  { id: 4, type: "coat", brownWeek: 35, brownRate: 1, whiteWeek: 36, whiteRate: 1 },
+];
+const stableClimate = (week) => (week % 52 < 18 || week % 52 >= 36) ? 0 : 1;
+const variableClimate = (week) => stableClimate(week);
+const generationEvery18Weeks = (week) => week % 18 === 0;
+
+// configure the presented presets for the simulation, which can be tweaked after selection as well.
+const simulationConfig = {
+  scenarios: [
+    {
+      name: "Scenario 1",
+      description: "No selection, large population, and a stable climate.",
+      options: {
+        selection: false,
+        startWeek: 0,
+        availableAlleles: basicAlleles,
+        carryingCapacity: 1000,
+        baseSurvivalRate: 0.96,
+        mismatchPenalty: 0.07,
+        calculateSnowCoverage: stableClimate,
+        shouldGenerateNewPopulation: generationEvery18Weeks,
+      }
+    },
+    {
+      name: "Scenario 2",
+      description: "No selection, small population, and a stable climate.",
+      options: {
+        selection: false,
+        startWeek: 0,
+        availableAlleles: basicAlleles,
+        carryingCapacity: 20,
+        baseSurvivalRate: 0.96,
+        mismatchPenalty: 0.07,
+        calculateSnowCoverage: stableClimate,
+        shouldGenerateNewPopulation: generationEvery18Weeks,
+      }
+    },
+    {
+      name: "Scenario 3",
+      description: "Selection, large population, and a stable climate.",
+      options: {
+        selection: true,
+        startWeek: 0,
+        availableAlleles: basicAlleles,
+        carryingCapacity: 1000,
+        baseSurvivalRate: 0.96,
+        mismatchPenalty: 0.07,
+        calculateSnowCoverage: stableClimate,
+        shouldGenerateNewPopulation: generationEvery18Weeks,
+      }
+    },
+    {
+      name: "Scenario 4",
+      description: "Selection, small population, and a stable climate.",
+      options: {
+        selection: true,
+        startWeek: 0,
+        availableAlleles: basicAlleles,
+        carryingCapacity: 20,
+        baseSurvivalRate: 0.96,
+        mismatchPenalty: 0.07,
+        calculateSnowCoverage: stableClimate,
+        shouldGenerateNewPopulation: generationEvery18Weeks,
+      }
+    },
+    {
+      name: "Scenario 5",
+      description: "Selection, large population, and a variable climate.",
+      options: {
+        selection: true,
+        startWeek: 0,
+        availableAlleles: basicAlleles,
+        carryingCapacity: 1000,
+        baseSurvivalRate: 0.96,
+        mismatchPenalty: 0.07,
+        calculateSnowCoverage: stableClimate,
+        shouldGenerateNewPopulation: generationEvery18Weeks,
+      }
+    },
+    {
+      name: "Scenario 6",
+      description: "Selection, small population, and a variable climate.",
+      options: {
+        selection: true,
+        startWeek: 0,
+        availableAlleles: basicAlleles,
+        carryingCapacity: 20,
+        baseSurvivalRate: 0.96,
+        mismatchPenalty: 0.07,
+        calculateSnowCoverage: variableClimate,
+        shouldGenerateNewPopulation: generationEvery18Weeks,
+      }
+    },
+  ]
+}
+export {
+  simulationConfig,
+  basicAlleles
+}
